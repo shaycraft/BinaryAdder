ddbb7901f556b811fae4eedcc2169bc1a1fc73fd
Sam Haycraft <shaycraft@metlife.com>
2018-05-09 17:33:31 -0600

    Implements bstring class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace BinaryAdder
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BAdder adder = new BAdder("101", "11");
+
+            Console.WriteLine(adder.AddIt());
         }
     }
 }
